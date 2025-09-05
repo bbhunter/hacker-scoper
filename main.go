c6346266e94aea7b4c8d75d133e492941e3ec642
ItsIgnacioPortal <5990@protonmail.com>
2025-09-05 19:04:25 -0300

    chore: Removed unused function popLine()

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -594,52 +593,6 @@ func main() {
 	}
 	cleanup()
 
-}
-
-// https://stackoverflow.com/a/30948278/11490425
-func popLine(f *os.File) ([]byte, error) {
-	fi, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-	buf := bytes.NewBuffer(make([]byte, 0, fi.Size()))
-
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		return nil, err
-	}
-	_, err = io.Copy(buf, f)
-	if err != nil {
-		return nil, err
-	}
-
-	line, err := buf.ReadBytes('\n')
-	if err != nil && err != io.EOF {
-		return nil, err
-	}
-
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		return nil, err
-	}
-	nw, err := io.Copy(f, buf)
-	if err != nil {
-		return nil, err
-	}
-	err = f.Truncate(nw)
-	if err != nil {
-		return nil, err
-	}
-	err = f.Sync()
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil {
-		return nil, err
-	}
-	return line, nil
 }
 
 func updateFireBountyJSON() {
