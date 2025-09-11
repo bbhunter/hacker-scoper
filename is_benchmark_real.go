16e22f8fb2538865bc25288d5fa8620a392871a0
ItsIgnacioPortal <5990@protonmail.com>
2025-09-11 18:41:36 -0300

    feat(tests): Added heap benchmark

--- is_benchmark_real.go
+++ is_benchmark_real.go
@@ -4,25 +4,47 @@ package main
 
 import (
 	"os"
+	"runtime"
 	"runtime/pprof"
 )
 
-var f *os.File
+var cpufile *os.File
+var ramfile *os.File
 
 func StartBenchmark() bool {
-	f, err := os.Create(`.\benchmarking\profiling-output\cpu.prof`)
+	cpufile, err := os.Create(`.\benchmarking\profiling-output\cpu.prof`)
 	if err != nil {
 		crash("could not create CPU profile: ", err)
 	}
-	err = pprof.StartCPUProfile(f)
+
+	ramfile, err = os.Create(`.\benchmarking\profiling-output\ram.prof`)
+	if err != nil {
+		crash("could not create CPU profile: ", err)
+	}
+
+	err = pprof.StartCPUProfile(cpufile)
 	if err != nil {
 		crash("could not start CPU profile: ", err)
 	}
+
 	return true
 }
 
 func StopBenchmark() bool {
 	pprof.StopCPUProfile()
-	f.Close()
+	cpufile.Close()
+
+	// Get a RAM profile
+	runtime.GC() // get up-to-date statistics
+	// Lookup("allocs") creates a profile similar to go test -memprofile.
+	// Alternatively, use Lookup("heap") for a profile
+	// that has inuse_space as the default index.
+	err := pprof.Lookup("allocs").WriteTo(ramfile, 0)
+	if err != nil {
+		crash("could not write memory profile: ", err)
+	}
+
+	ramfile.Close()
+
 	return true
 }
